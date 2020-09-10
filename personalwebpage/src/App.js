f004349201a32c1ccc82e7b72d44d61d971d0f52
RandonSRProctor <randyproctor@gmail.com>
2020-09-10 14:26:58 -0400

    Set up basic state

--- personalwebpage/src/App.js
+++ personalwebpage/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Randy Proctor',
+      headerLinks: [
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: 'Contact', path: '/contact' }
+      ],
+      home: {
+        title: 'Born to Run',
+        subtitle: 'Touring Rock Musician turned Developer',
+        text: 'See my projects below'
+      },
+      about: {
+        title: 'About Me',
+      },
+      contact: {
+        title: "Let's talk",
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div>Hello World!</div>
+    );
+  }
 }
 
 export default App;
