d0ae79b3fab82782f69f611fd3d018a58fe10c52
RandonSRProctor <randyproctor@gmail.com>
2020-09-14 16:39:49 -0400

    created Hero component and added to HomePage

--- personalwebpage/src/pages/HomePage.js
+++ personalwebpage/src/pages/HomePage.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Hero from '../components/Hero';
 
 function HomePage(props) {
 
     return (
-        <p>Home Works!</p>
+        <Hero 
+            title={props.title}
+            subtitle={props.subtitle} 
+            text={props.text}
+        />
     );
 
 }
