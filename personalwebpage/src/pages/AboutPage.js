3ff9fba53afcf22ffafc8ff0a8f652986172a41b
RandonSRProctor <randyproctor@gmail.com>
2020-09-16 15:20:00 -0400

    Set up About Me page and components

--- personalwebpage/src/pages/AboutPage.js
+++ personalwebpage/src/pages/AboutPage.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Content from '../components/Content';
+import Hero from '../components/Hero';
 
 function AboutPage(props) {
 
     return (
-        <p>AboutPage Works!</p>
+        <div>
+            <Hero 
+            title={props.title}
+            />
+
+            <Content>
+                <p>Randy Proctor is a bad dude.</p>
+                <p>And not the bad kind of bad.</p>
+                <p>When I say bad, I mean good.</p>
+                <p>And when I say good, I mean <i>bad to the bone.</i></p>
+            </Content>
+        </div>
     );
 
 }
